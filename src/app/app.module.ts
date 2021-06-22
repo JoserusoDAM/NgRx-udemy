53adc1f7ac3ab6218c3f2135ca8cd08a25564a3d
Jose Ruso <joserusoalvarez@gmail.com>
2021-06-22 12:12:50 +0200

    Primer reducer terminaod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
-import { TodoModule } from './todos/todo.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { TodoModule } from './todos/todo.module';
+import { environment } from './../environments/environment';
+
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todos/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +20,13 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    TodoModule
+    TodoModule,
+    StoreModule.forRoot({ todos: todoReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,51 @@
+import { crear, toggle, editar, borrar, toggleAll } from './todo.actions';
+import { Action, createReducer, on } from '@ngrx/store';
+import { Todo } from './models/todo.model';
+
+const estadoInicial: Todo[] = [
+  new Todo('Salvar al mundo'),
+  new Todo('Vencer a Thanos'),
+  new Todo('Comprar traje de Ironman'),
+];
+
+const _todoReducer = createReducer(
+  estadoInicial,
+  on(crear, (state, { texto }) => [...state, new Todo(texto)]),
+  on(toggle, (state, { id }) => {
+    return state.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completado: !todo.completado,
+        };
+      } else {
+        return todo;
+      }
+    });
+  }),
+  on(editar, (state, { id, texto }) => {
+    return state.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          texto: texto,
+        };
+      } else {
+        return todo;
+      }
+    });
+  }),
+  on(borrar, (state, { id }) => state.filter((todo) => todo.id !== id)),
+  on(toggleAll, (state, { completado }) =>
+    state.map((todo) => {
+      return {
+        ...todo,
+        completado: completado,
+      };
+    })
+  )
+);
+
+export function todoReducer(state: Todo[] | undefined, action: Action) {
+  return _todoReducer(state, action);
+}
